3fdd2f193f27911d5236d9b882e583a3decb264f
nikita_mullovsky <mullovskii@gmail.com>
2017-11-29 18:16:53 +0300

    china5

--- db/seeds.rb
+++ db/seeds.rb
@@ -99,7 +99,7 @@
 # Inflow.where(exporter_country_id: mother_country.id).destroy_all
 
 
-	# mother_country = Country.where(name: "China").take
+	mother_country = Country.where(name: "China").take
 		Traffic.where(mother_country_id: mother_country.id).each do |traffic|
 			if traffic.website.valid_for_yandex
 				if inflow = Inflow.where(exporter_country_id: mother_country.id, importer_country_id: traffic.country_id).take
