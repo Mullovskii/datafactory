5cf7ee817cfcfc36217bafd7ca1f8c175a0b157a
nikita_mullovsky <mullovskii@gmail.com>
2017-12-01 13:30:36 +0300

    germany

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,35 +49,35 @@
 
 
 # SEED WEBSITES
-	# file = File.join(Rails.root, 'app', 'files', 'hongkong', 'TopSites-(Shopping)--(344)--(Month_2017_10_1).xlsx')
- #    xlsx = Roo::Spreadsheet.open(file)
- #    xlsx = Roo::Excelx.new(file)
- #    xlsx.sheet(1).column(1).each do |site|
- #    	unless site == 'Domain' || Website.where(url: site.downcase).take
- #    		Website.create!(url: site.downcase, valid_for_yandex: true, category: 0, status: 1, country_id: Country.where(name: "Hong Kong").take.id)
- #    		if File.join(Rails.root, 'app', 'files', 'hongkong', "GeographyExtended-(#{site.downcase})--(999)--(Month_2017_10_1).xlsx")
- #    			file = File.join(Rails.root, 'app', 'files', 'hongkong', "GeographyExtended-(#{site.downcase})--(999)--(Month_2017_10_1).xlsx")
- #    			p file
- #    			unless file == ''
-	# 			    xlsx = Roo::Spreadsheet.open(file)
-	# 			    xlsx = Roo::Excelx.new(file)
-	# 			   	Website.where(url: site.downcase).take.update(monthly_visits: xlsx.sheet(1).cell(1,'J'))
-	# 			   	website = Website.where(url: site.downcase).take
-	# 			    hash = xlsx.sheet(1).each(country: 'Country', share: 'Traffic share') do |hash|
-	# 			    	unless Country.where(name: hash[:country]).take || hash[:country] == 'Country'
-	# 			    		Country.create!(name: hash[:country])
-	# 			    	end	    
-	# 			    	unless Traffic.where(website_id: website.id, country_id: Country.where(name: hash[:country]).take).take 
-	# 			    		unless hash[:country] == 'Country' || website.url == 'Domain' 
-	# 			    			traffic = Traffic.create!(mother_country_id: website.country.id, website_id: website.id, country_id: Country.where(name: hash[:country]).take.id, country_share: hash[:share])
-	# 			    			traffic.update!(country_visits: (traffic.country_share*website.monthly_visits*12), annual_turnover: (traffic.country_share*website.monthly_visits*Setting.last.average_bill*Setting.last.conversion*12))	
-	# 			    		end
-	# 			    	end
-	# 			    end
-	# 			end
- #    		end    		
- #    	end
- #    end
+	file = File.join(Rails.root, 'app', 'files', 'germany', 'TopSites-(Shopping)--(276)--(Month_2017_10_1).xlsx')
+    xlsx = Roo::Spreadsheet.open(file)
+    xlsx = Roo::Excelx.new(file)
+    xlsx.sheet(1).column(1).each do |site|
+    	unless site == 'Domain' || Website.where(url: site.downcase).take
+    		Website.create!(url: site.downcase, valid_for_yandex: true, category: 0, status: 1, country_id: Country.where(name: "Germany").take.id)
+    		if File.join(Rails.root, 'app', 'files', 'germany', "GeographyExtended-(#{site.downcase})--(999)--(Month_2017_10_1).xlsx")
+    			file = File.join(Rails.root, 'app', 'files', 'germany', "GeographyExtended-(#{site.downcase})--(999)--(Month_2017_10_1).xlsx")
+    			p file
+    			unless file == ''
+				    xlsx = Roo::Spreadsheet.open(file)
+				    xlsx = Roo::Excelx.new(file)
+				   	Website.where(url: site.downcase).take.update(monthly_visits: xlsx.sheet(1).cell(1,'J'))
+				   	website = Website.where(url: site.downcase).take
+				    hash = xlsx.sheet(1).each(country: 'Country', share: 'Traffic share') do |hash|
+				    	unless Country.where(name: hash[:country]).take || hash[:country] == 'Country'
+				    		Country.create!(name: hash[:country])
+				    	end	    
+				    	unless Traffic.where(website_id: website.id, country_id: Country.where(name: hash[:country]).take).take 
+				    		unless hash[:country] == 'Country' || website.url == 'Domain' 
+				    			traffic = Traffic.create!(mother_country_id: website.country.id, website_id: website.id, country_id: Country.where(name: hash[:country]).take.id, country_share: hash[:share])
+				    			traffic.update!(country_visits: (traffic.country_share*website.monthly_visits*12), annual_turnover: (traffic.country_share*website.monthly_visits*Setting.last.average_bill*Setting.last.conversion*12))	
+				    		end
+				    	end
+				    end
+				end
+    		end    		
+    	end
+    end
 
 
 	
@@ -95,18 +95,18 @@
 	# 	end
 	# end
 
-mother_country = Country.where(name: "Japan").take
-Inflow.where(exporter_country_id: mother_country.id).destroy_all
+# mother_country = Country.where(name: "Japan").take
+# Inflow.where(exporter_country_id: mother_country.id).destroy_all
 
 
-	mother_country = Country.where(name: "Japan").take
-		Traffic.where(mother_country_id: mother_country.id).each do |traffic|
-			if traffic.website.valid_for_yandex
-				if inflow = Inflow.where(exporter_country_id: mother_country.id, importer_country_id: traffic.country_id).take
-					inflow.update!(annual_turnover: inflow.annual_turnover + traffic.annual_turnover)
-				else
-					Inflow.create!(exporter_country_id: mother_country.id, importer_country_id: traffic.country_id, annual_turnover: traffic.annual_turnover)
-				end
-			end
-		end
+	# mother_country = Country.where(name: "Germany").take
+	# 	Traffic.where(mother_country_id: mother_country.id).each do |traffic|
+	# 		if traffic.website.valid_for_yandex
+	# 			if inflow = Inflow.where(exporter_country_id: mother_country.id, importer_country_id: traffic.country_id).take
+	# 				inflow.update!(annual_turnover: inflow.annual_turnover + traffic.annual_turnover)
+	# 			else
+	# 				Inflow.create!(exporter_country_id: mother_country.id, importer_country_id: traffic.country_id, annual_turnover: traffic.annual_turnover)
+	# 			end
+	# 		end
+	# 	end
 
