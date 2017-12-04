1a38c9ece4b1942d6a2958a2f626d3f46cd51359
nikita_mullovsky <mullovskii@gmail.com>
2017-12-04 17:29:00 +0300

    vietnam1

--- app/models/website.rb
+++ app/models/website.rb
@@ -9,12 +9,21 @@ class Website < ApplicationRecord
   enum status: [ 'Invalid', 'Awaiting', 'Pipelined', 'Underway', 'Boarded']
 
   after_create :generate_setup
+  after_update :renew_traffic
 
 
 
   def generate_setup
   	setting = Setting.last
   	Setup.create(website_id: self.id, conversion: setting.conversion, average_bill: setting.average_bill, yandex_net_fee: setting.yandex_net_fee, yandex_share: setting.yandex_share)
+  end
+
+  def renew_traffic
+    self.traffics.each do |traffic|
+      unless traffic.mother_country_id == self.country_id
+        traffic.update!(mother_country_id: self.country_id)
+      end
+    end
   end
 
 end
